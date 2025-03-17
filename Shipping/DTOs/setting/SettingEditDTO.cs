27243f738cbd89583e27309b4a8c5db9f00bfde9
’Mona Ali <m463414@gmail.com>
2025-03-17 16:50:30 +0200

    monasetting

---
+++ Shipping/DTOs/setting/SettingEditDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Shipping.DTOs.setting
+{
+    public class SettingEditDTO
+    {  
+        public int Id { get; set; }
+        public decimal ShippingToVillageCost { get; set; }
+        public bool DeliveryAutoAccept { get; set; } = false;
+    }
+}
