245eb711fb3bc37b8eb4aa61c128651ee69e94ea
MohabYounis <167039645+MohabYounis@users.noreply.github.com>
2025-03-18 02:17:19 +0200

    SettingMerchantBranch

--- Shipping/Models/Product.cs
+++ Shipping/Models/Product.cs
@@ -5,12 +5,12 @@ namespace Shipping.Models
     public class Product
     {
         public int Id { get; set; }
-        [ForeignKey("product")]
-        public int Product_Id { get; set; }
+        [ForeignKey("Order")]
+        public int Order_Id { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
         public float ItemWeight { get; set; }
         public bool IsDeleted { get; set; } = false;
-        public virtual Product? product { get; set; }
+        public virtual Order? Order { get; set; }
     }
 }
