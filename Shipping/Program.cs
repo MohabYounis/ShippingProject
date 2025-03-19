48687c167c0fe46dae761bd2d57487b152e0d207
Shereen Ahmed <Shereenahmed387@gmail.com>
2025-03-19 15:24:52 +0200

    test1

--- Shipping/Program.cs
+++ Shipping/Program.cs
@@ -64,7 +64,7 @@ namespace Shipping
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger(); // ? ??? ????? ??? ????
+                app.UseSwagger(); 
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shipping API V1"));
             }
 
