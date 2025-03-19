1864a21fb85f9076f16ff121691c1bfd0f941630
ammarallam322 <ammarallam322@gmail.com>
2025-03-19 17:46:10 +0200

    editting

--- Shipping/Controllers/EmployeeController.cs
+++ Shipping/Controllers/EmployeeController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Shipping.DTOs.Employee;
-using Shipping.DTOs.EmployeeDTOs;
 using Shipping.Models;
 using Shipping.Services;
 using Shipping.Services.IModelService;
--- Shipping/Services/IModelService/IMerchantService.cs
+++ Shipping/Services/IModelService/IMerchantService.cs
@@ -4,8 +4,6 @@ namespace Shipping.Services.IModelService
 {
     public interface IMerchantService : IServiceGeneric<Merchant>
     {
-        //Task<IEnumerable<Merchant>> GetAllAsync();
-        //Task<IEnumerable<Merchant>> GetAllExistAsync();
-        //Task<Merchant> GetByIdAsync(int id);
+       
     }
 }
