efdf13cc31c7f9586227713cb5b2985b0235be03
MohabYounis <167039645+MohabYounis@users.noreply.github.com>
2025-03-30 17:33:22 +0200

    editDeliveryDTOs

---
+++ Shipping/DTOs/DeliveryDTOs/DeliveryCreateDTO.cs
@@ -0,0 +1,39 @@
+﻿using Shipping.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shipping.DTOs.DeliveryDTOs
+{
+    public class DeliveryCreateDTO
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 10 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression(@"^(?:\+20|0)?1[0-2,5,1]{1}[0-9]{8}$", ErrorMessage = "Invalid Egyptian phone number format.")]
+        public string Phone { get; set; }
+        //password 
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Branch ID is required.")]
+        public int BranchId { get; set; }
+
+        [Required(ErrorMessage = "At least one government ID is required.")]
+        public List<int> GovernmentsId { get; set; }
+
+        [Required(ErrorMessage = "Choose a discount type.")]
+        public string DiscountType { get; set; }                                        //added by Mohab
+
+        [Required(ErrorMessage = "Select the company percentage from order.")]
+        public decimal CompanyPercentage { get; set; }                                  //added by Mohab
+    }
+
+}
+
