b7727a993b17ca5334350fd172ebabfac7feda4a
MonaAlii123 <m463414@gmail.com>
2025-04-08 17:05:51 +0200

    monadtosssssss

--- Shipping/DTOs/ShippingType/CreateShippingTypeDTO.cs
+++ Shipping/DTOs/ShippingType/CreateShippingTypeDTO.cs
@@ -1,10 +1,17 @@
-﻿namespace Shipping.DTOs.ShippingType
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shipping.DTOs.ShippingType
 {
     public class CreateShippingTypeDTO
     {
-
+        [Required(ErrorMessage = "Shipping type is required.")]
+      
         public string Type { get; set; }
+
+        [StringLength(150)]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Shipping type cost is required.")]
         public decimal Cost { get; set; }
 
     }
