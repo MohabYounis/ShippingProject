eed9f02fafa1458bb65c2abe977be92c025a7dc2
MohabYounis <167039645+MohabYounis@users.noreply.github.com>
2025-04-19 21:46:45 +0200

    workingOnMultiRolesInOrder

--- Shipping/Services/ModelService/OrderService.cs
+++ Shipping/Services/ModelService/OrderService.cs
@@ -128,7 +128,7 @@ namespace Shipping.Services.ModelService
         public async Task<IEnumerable<Order>> GetAllExistByDeliveryByStatus(int id, string orderStatus)
         {
             var orders = await GetAllExistAsync();
-            var ordersByDeliver = orders.Where(o => o.Delivery.Id == id).ToList();
+            var ordersByDeliver = orders.Where(o => o.Delivery_Id == id).ToList();
             var ordersByStatus = new List<Order>();
 
             if(Enum.TryParse<OrderStatus>(orderStatus, true, out var orderStatusVariable))
